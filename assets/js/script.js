206b45b4d5a0348a6b8a7ba27c6377254cb81cb9
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-07-13 12:27:34 +0200

    Fonctionnalité de sauvegarde des données

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-var count = 0
-var multiplicator = 1
-var perSecond = 0
-var perClick = 1
+var count = localStorage.getItem("clicker_count") ? new Number(localStorage.getItem("clicker_count")) : 0
+var multiplicator = localStorage.getItem("clicker_multiplicator") ? new Number(localStorage.getItem("clicker_multiplicator")) : 1
+var perSecond = localStorage.getItem("clicker_perseconde") ? new Number(localStorage.getItem("clicker_perseconde")) : 0
+var perClick = localStorage.getItem("clicker_perclick") ? new Number(localStorage.getItem("clicker_perclick")) : 1
 var bonusPrice = 2500
 var isBonusActive = false
-var bonusTimer = 0
-var multiplicatorLvl = 1
-var autoclickLvl = 1
+var multiplicatorLvl = localStorage.getItem("clicker_multiplicatorlvl") ? new Number(localStorage.getItem("clicker_multiplicatorlvl")) : 1
+var autoclickLvl = localStorage.getItem("clicker_autoclicklvl") ? new Number(localStorage.getItem("clicker_autoclicklvl")) : 1
 var bonusTimer = 0
 var countElement = document.getElementById("count")
 var messageElement = document.getElementById("message")
@@ -20,25 +19,60 @@ var buttonBonusElement = document.getElementById("bonusButton")
 var multiplicatorLvlElement = document.getElementById("multiplicatorlvl")
 var autoclickLvlElement = document.getElementById("autoclicklvl")
 
+function initElement() {
+    countElement.innerText = count
+
+    buttonMultiplicatorElement.innerText = "Améliorer pour " + calcMultiplicatorPrice() + " crottes"
+    multiplicatorElement.innerText = "x" + multiplicator
+    multiplicatorLvlElement.innerText = multiplicatorLvl
+
+    buttonAutoclickElement.innerText = "Améliorer pour " + calcAutoClickPrice() + " crottes"
+    perSecondElement.innerText = perSecond * multiplicator
+    autoclickLvlElement.innerText = autoclickLvl
+}
+
+function reset() {
+    multiplicatorLvl = 1
+    multiplicator = 1
+    autoclickLvl = 1
+    perSecond = 0
+    count = 0
+
+    localStorage.setItem("clicker_multiplicatorlvl", multiplicatorLvl)
+    localStorage.setItem("clicker_multiplicator", multiplicator)
+    localStorage.setItem("clicker_autoclicklvl", autoclickLvl)
+    localStorage.setItem("clicker_perseconde", perSecond)
+
+    initElement()
+}
+
 function changeCount(increment) {
     count += increment
     countElement.innerText = count
+    localStorage.setItem("clicker_count", count)
 }
 
 function changeMultiplicator(value) {
     multiplicatorLvl++
     multiplicator += value
-    buttonMultiplicatorElement.innerText = "Multiplicateur : " + calcMultiplicatorPrice() + " crotte"
+    buttonMultiplicatorElement.innerText = "Améliorer pour " + calcMultiplicatorPrice() + " crottes"
     multiplicatorElement.innerText = "x" + multiplicator
     multiplicatorLvlElement.innerText = multiplicatorLvl
+    perSecondElement.innerText = perSecond * multiplicator
+
+    localStorage.setItem("clicker_multiplicatorlvl", multiplicatorLvl)
+    localStorage.setItem("clicker_multiplicator", multiplicator)
 }
 
 function changePerSecond(value) {
     autoclickLvl++
     perSecond += value
-    buttonAutoclickElement.innerText = "AutoClick : " + calcAutoClickPrice() + " crotte"
-    perSecondElement.innerText = perSecond
+    buttonAutoclickElement.innerText = "Améliorer pour " + calcAutoClickPrice() + " crottes"
+    perSecondElement.innerText = perSecond * multiplicator
     autoclickLvlElement.innerText = autoclickLvl
+
+    localStorage.setItem("clicker_autoclicklvl", autoclickLvl)
+    localStorage.setItem("clicker_perseconde", perSecond)
 }
 
 function changeBonusTimer(increment) {
@@ -87,6 +121,11 @@ buttonAutoclickElement.addEventListener("click", () => {
 })
 
 buttonBonusElement.addEventListener("click", () => {
+    if (isBonusActive) {
+        sendmessage("Un bonus est déjà actif")
+        return
+    }
+
     if (count >= bonusPrice) {
         changeCount(-bonusPrice)
         sendmessage("Good vous avez acheté le bonus")
@@ -110,3 +149,5 @@ buttonBonusElement.addEventListener("click", () => {
 setInterval(() => {
     changeCount(perSecond * multiplicator)
 }, 1000)
+
+initElement()
